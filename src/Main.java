328b22633fc0bd0b91efffa81acec17d1d0e19f5
DanIT23 <dany2217@yahoo.com>
2023-05-20 19:50:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        int[]listNumbers = {0,1,5,7,9};
+        for(int i = 0; i < listNumbers.length; i++){
+            System.out.println(listNumbers[i]);
+        }
+        ArrayList<Integer>listOfNumbers = new ArrayList<>();
+        listOfNumbers.add(0);
+        listOfNumbers.add(1);
+        listOfNumbers.add(5);
+        listOfNumbers.add(7);
+        listOfNumbers.add(9);
+        System.out.println(listOfNumbers);
+        }
+    }
+
+
+
